b8f0c717e2df53521fbb7b04d7a681ce32f44396
nsrchemie <nsrchemie@gmail.com>
2016-01-10 18:38:54 -0500

    added regex to filter numerical and dash filled entries

--- sitings/views.py
+++ sitings/views.py
@@ -29,17 +29,17 @@ def post_detail(request, pk):
     BS_data = BeautifulSoup(plant_data, "lxml")
     decoded_data = BS_data.decode('utf-8')
     cleaned_data = decoded_data.split('<td>\n')
-    number_cleaner = re.compile("^\+?(\d+-?)*\d+$")
+    number_cleaner = re.compile("/^(\d+-?)+\d+$/")
     chem_raw = []
     chems = []
     for chem in cleaned_data:
         if '|' in chem:
             # if not number_cleaner.match(chem):
             chem_raw.append(chem.replace('</td>', '').replace('</n>', '').replace('<n>','').strip().split('|')[-1].encode('ascii','replace').decode('utf-8', 'ignore'))
-            # for value in chem_raw:
-            #     if number_cleaner.match(chem):
-            #         chems.remove(value)
-    return render(request, 'sitings/post_detail.html', {'post': post, 'chems':chem_raw})
+    for value in chem_raw:
+        if not re.match("^\+?(\d+-?)*\d+$", value):
+            chems.append(value)
+    return render(request, 'sitings/post_detail.html', {'post': post, 'chems':chems})
             # except:
             #     pass``
     # return render(request, 'sitings/post_detail.html', {'post': post, 'x': x})
