a5cff92d054f1363acd32206992ecc23dbdef3cd
nsrchemie <nsrchemie@gmail.com>
2016-01-10 17:53:06 -0500

    plant detail site

--- sitings/urls.py
+++ sitings/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
